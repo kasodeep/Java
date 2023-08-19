5b51d57a774da14a94972637849329b0037a5c33
Kasodariya Deep <deepkasodariya44@gmail.com>
2023-08-19 12:38:46 +0530

    Updated And Revised Time Complexity

---
+++ src/TimeAndSpace/Examples.java
@@ -0,0 +1,26 @@
+package TimeAndSpace;
+
+/*
+ * Time Complexity - Applying the asymptotic analysis to measure the time requirement of an algorithm as a function of input size is known as time complexity.
+ * Total time complexity of a program is equal to the summation of all the running time of disconnected fragments.
+ * Types - 1) Worst Case : Big O()
+           2) Best Case : Big Omega
+           3) Average Case : Big Theta
+ * Space Complexity - Space Complexity is the extra memory space requirement of an algorithm by applying asymptotic analysis.
+ */
+public class Examples {
+
+    public static void main(String[] args) {
+
+        // Single For Loop O(N)
+        // Nested Loops - O(N^2)
+        // for(int i = 0; i < N; i *= k) - O(log * (N)) to base k.
+
+        /*
+         * O(N * N^1/2)
+         * for(int i = 0; i < N; i++)
+         *    for(int i = 0; i < Math.sqrt(N), i++)
+         * */
+
+    }
+}
