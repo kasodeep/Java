e9e27e463b9e0b92c6961d0b15b0ed8441f4c965
Kasodariya Deep <deepkasodariya44@gmail.com>
2023-09-09 18:37:08 +0530

    Solved Two More Important Questions Of Stack

--- src/stacks/TheCelebrityProblem.java
+++ src/stacks/TheCelebrityProblem.java
@@ -3,39 +3,39 @@ package stacks;
 import java.util.Stack;
 
 public class TheCelebrityProblem {
-   int celebrity(int M[][], int n) {
-      Stack<Integer> st = new Stack<>();
-      for (int i = 0; i < n; i++) {
-         st.push(i);
-      }
+    int celebrity(int[][] M, int n) {
+        Stack<Integer> st = new Stack<>();
+        for (int i = 0; i < n; i++) {
+            st.push(i);
+        }
 
-      while (st.size() > 1) {
-         int v1 = st.pop();
-         int v2 = st.pop();
+        while (st.size() > 1) {
+            int v1 = st.pop();
+            int v2 = st.pop();
 
-         if (M[v1][v2] == 0) { // v1 shayad hero ho
-            st.push(v1);
-         } else if (M[v2][v1] == 0) { // v2 shayad hero ho
-            st.push(v2);
-         }
-      }
+            if (M[v1][v2] == 0) { // v1 might be a hero.
+                st.push(v1);
+            } else if (M[v2][v1] == 0) { // v2 might be a hero.
+                st.push(v2);
+            }
+        }
 
-      if (st.size() == 0)
-         return -1;
-
-      int potential = st.pop();
-      for (int j = 0; j < n; j++) {
-         if (M[potential][j] == 1)
+        if (st.size() == 0)
             return -1;
-      }
 
-      for (int i = 0; i < n; i++) {
-         if (i == potential)
-            continue;
-         if (M[i][potential] == 0)
-            return -1;
-      }
+        int potential = st.pop();
+        for (int j = 0; j < n; j++) {
+            if (M[potential][j] == 1)
+                return -1;
+        }
 
-      return potential;
-   }
+        for (int i = 0; i < n; i++) {
+            if (i == potential)
+                continue;
+            if (M[i][potential] == 0)
+                return -1;
+        }
+
+        return potential;
+    }
 }
