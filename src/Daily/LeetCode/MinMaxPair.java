f1fa2fc7e0cdc42496d1ef7b81f6ad72a35a856a
Kasodariya Deep <deepkasodariya44@gmail.com>
2023-11-18 11:33:38 +0530

    Daily Troubles Continued!

---
+++ src/Daily/LeetCode/MinMaxPair.java
@@ -0,0 +1,20 @@
+package Daily.LeetCode;
+
+import java.util.Arrays;
+
+public class MinMaxPair {
+
+    public static int minPairSum(int[] nums) {
+        Arrays.sort(nums);
+        int maxSum = 0;
+
+        for (int i = 0; i < nums.length / 2; i++) {
+            maxSum = Math.max(maxSum, nums[i] + nums[nums.length - 1 - i]);
+        }
+        return maxSum;
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
