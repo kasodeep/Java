05a0a3a587efa76099c625c2dc1730149a01d67b
Kasodariya Deep <106021230+kasodeep@users.noreply.github.com>
2023-12-26 10:39:15 +0530

    It's Getting Tough!💀

---
+++ src/ArraysD2/SubMatrixSumZero.java
@@ -0,0 +1,59 @@
+package ArraysD2;
+
+import java.util.ArrayList;
+
+public class SubMatrixSumZero {
+
+    public static ArrayList<ArrayList<Integer>> sumZeroMatrix(int[][] a) {
+        int n = a.length;
+        int m = a[0].length;
+
+        int[][] prefixSum = new int[n][m];
+
+        // Calculating the Prefix Sum.
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                prefixSum[i][j] = a[i][j];
+                if (i > 0) prefixSum[i][j] += prefixSum[i - 1][j];
+                if (j > 0) prefixSum[i][j] += prefixSum[i][j - 1];
+                if (i > 0 && j > 0) prefixSum[i][j] -= prefixSum[i - 1][j - 1];
+            }
+        }
+
+        int maxSize = 0;
+        ArrayList<ArrayList<Integer>> result = new ArrayList<>();
+
+        for (int c1 = 0; c1 < m; c1++) {
+            for (int c2 = c1; c2 < m; c2++) {
+                for (int r1 = 0; r1 < n; r1++) {
+                    for (int r2 = r1; r2 < n; r2++) {
+
+                        int sum = prefixSum[r2][c2];
+                        if (r1 > 0) sum -= prefixSum[r1 - 1][c2];
+                        if (c1 > 0) sum -= prefixSum[r2][c1 - 1];
+                        if (r1 > 0 && c1 > 0) sum += prefixSum[r1 - 1][c1 - 1];
+
+                        int area = (r2 - r1 + 1) * (c2 - c1 + 1);
+                        if (sum == 0 && area > maxSize) {
+                            maxSize = area;
+                            result.clear();
+                            for (int i = r1; i <= r2; i++) {
+                                ArrayList<Integer> row = new ArrayList<>();
+                                for (int j = c1; j <= c2; j++) {
+                                    row.add(a[i][j]);
+                                }
+                                result.add(row);
+                            }
+                        }
+                    }
+                }
+            }
+        }
+
+        return result;
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
