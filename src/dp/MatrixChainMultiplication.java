b7fe7d8afaf19887e2676c90f74b499b66631015
Kasodariya Deep <106021230+kasodeep@users.noreply.github.com>
2024-01-27 15:35:28 +0530

    DP Expanding!

---
+++ src/dp/MatrixChainMultiplication.java
@@ -0,0 +1,26 @@
+package dp;
+
+import java.util.Arrays;
+
+public class MatrixChainMultiplication {
+
+    static int[][] dp;
+
+    static int matrixMultiplication(int n, int[] p) {
+        dp = new int[n + 1][n + 1];
+        for (int[] temp : dp) Arrays.fill(temp, -1);
+        return mcm(p, 1, n - 1);
+    }
+
+    static int mcm(int[] p, int i, int j) {
+        if (i >= j) return 0;
+
+        if (dp[i][j] != -1) return dp[i][j];
+        int ans = Integer.MAX_VALUE;
+        for (int k = i; k <= j - 1; k++) {
+            int temp = mcm(p, i, k) + mcm(p, k + 1, j) + (p[i - 1] * p[k] * p[j]);
+            ans = Math.min(ans, temp);
+        }
+        return dp[i][j] = ans;
+    }
+}
---
+++ src/dp/PalindromicPartitioning.java
@@ -0,0 +1,40 @@
+package dp;
+
+import java.util.Arrays;
+
+public class PalindromicPartitioning {
+
+    private static int[][] dp;
+
+    static int palindromicPartition(String str) {
+        dp = new int[501][501];
+        for (int[] temp : dp) Arrays.fill(temp, -1);
+        return helper(str, 0, str.length() - 1);
+    }
+
+    private static boolean isPalindrome(String s, int i, int j) {
+        while (i <= j) if (s.charAt(i++) != s.charAt(j--)) return false;
+        return true;
+    }
+
+    private static int helper(String s, int i, int j) {
+        if (i >= j) return 0;
+        if (dp[i][j] != -1) return dp[i][j];
+        if (isPalindrome(s, i, j)) return 0;
+
+        int mini = Integer.MAX_VALUE;
+        for (int k = i; k < j; k++) {
+            int left, right;
+
+            if (dp[i][k] != -1) left = dp[i][k];
+            else left = helper(s, i, k);
+
+            if (dp[k + 1][j] != -1) right = dp[k + 1][j];
+            else right = helper(s, k + 1, j);
+
+            int ans = 1 + left + right;
+            mini = Math.min(mini, ans);
+        }
+        return dp[i][j] = mini;
+    }
+}
