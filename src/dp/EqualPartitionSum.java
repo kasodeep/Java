15704fb043f77708abb60e0fdbf320fc3051db45
Kasodariya Deep <106021230+kasodeep@users.noreply.github.com>
2024-01-30 17:51:49 +0530

    Getting Hold Of DP!

---
+++ src/dp/EqualPartitionSum.java
@@ -0,0 +1,30 @@
+package dp;
+
+import java.util.Arrays;
+
+public class EqualPartitionSum {
+
+    static int[][] dp;
+
+    static int equalPartition(int N, int[] arr) {
+        int sum = 0;
+        for (int i = 0; i < N; i++) sum += arr[i];
+        if (sum % 2 != 0) return 0;
+
+        dp = new int[N + 1][sum / 2 + 1];
+        for (int[] temp : dp) Arrays.fill(temp, -1);
+
+        int ans = solve(0, sum / 2, N, arr);
+        return ans == -1 ? 0 : ans;
+    }
+
+    static int solve(int i, int sum, int N, int[] arr) {
+        if (i == N) return 0;
+        if (sum < 0) return 0;
+        if (sum == 0) return 1;
+
+        if (dp[i][sum] != -1) return dp[i][sum];
+        dp[i][sum] = solve(i + 1, sum - arr[i], N, arr) | solve(i + 1, sum, N, arr);
+        return dp[i][sum];
+    }
+}
---
+++ src/dp/KnapsackZeroOne.java
@@ -0,0 +1,27 @@
+package dp;
+
+import java.util.Arrays;
+
+public class KnapsackZeroOne {
+
+    static int[][] memo;
+
+    static int knapSack(int W, int[] wt, int[] val, int n) {
+        memo = new int[n + 1][W + 1];
+        for (int[] arr : memo) Arrays.fill(arr, -1);
+
+        int ans = solve(wt, val, 0, W, n);
+        return ans == -1 ? 0 : ans;
+    }
+
+    static int solve(int[] wt, int[] val, int i, int W, int n) {
+        if (i >= n) return 0;
+        if (W <= 0) return 0;
+
+        if (memo[i][W] != -1) return memo[i][W];
+        if (W - wt[i] >= 0) memo[i][W] = val[i] + solve(wt, val, i + 1, W - wt[i], n);
+
+        memo[i][W] = Math.max(memo[i][W], solve(wt, val, i + 1, W, n));
+        return memo[i][W];
+    }
+}
