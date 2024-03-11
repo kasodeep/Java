f3cef6b3c7ef8b6f77d6b121c52caedf2d5b79c3
Kasodariya Deep <106021230+kasodeep@users.noreply.github.com>
2024-03-11 16:43:38 +0530

    King Life!👑

--- src/BackTracking/NQueens.java
+++ src/BackTracking/NQueens.java
@@ -2,100 +2,94 @@ package BackTracking;
 
 public class NQueens {
 
-   /*
-    * Helper Function to find if there is a possible combination to fint the
-    * arrangement.
-    */
-   public static void nqueen(char[][] board, int row) {
-      int n = board.length;
+    /*
+     * Helper Function to find if there is a possible combination to find the arrangement.
+     */
+    public static void nQueen(char[][] board, int row) {
+        int n = board.length;
 
-      // Print the solution, Base case
-      if (row == n) {
-         for (int i = 0; i < board.length; i++) {
-            for (int j = 0; j < board.length; j++) {
-               System.out.print(board[i][j]);
+        if (row == n) {
+            for (char[] chars : board) {
+                for (int j = 0; j < board.length; j++) System.out.print(chars[j]);
+                System.out.println();
             }
             System.out.println();
-         }
-         System.out.println();
-         return;
-      }
+            return;
+        }
 
-      // Iterate over the board row.
-      for (int j = 0; j < n; j++) {
-         if (isSafe(board, row, j)) {
-            board[row][j] = 'Q';
-            nqueen(board, row + 1);
-            board[row][j] = 'X'; // backtracking
-         }
-      }
-   }
+        // Iterate over the board row.
+        for (int j = 0; j < n; j++) {
+            if (isSafe(board, row, j)) {
+                board[row][j] = 'Q';
+                nQueen(board, row + 1);
+                board[row][j] = 'X';
+            }
+        }
+    }
 
-   /*
-    * Helper function to check if the queen is safe to place.
-    */
-   public static boolean isSafe(char[][] board, int row, int col) {
+    /*
+     * Helper function to check if the queen is safe to place.
+     */
+    public static boolean isSafe(char[][] board, int row, int col) {
 
-      // Horizontal
-      for (int j = 0; j < board.length; j++) {
-         if (board[row][j] == 'Q') {
-            return false;
-         }
-      }
+        // Horizontal
+        for (int j = 0; j < board.length; j++) {
+            if (board[row][j] == 'Q') {
+                return false;
+            }
+        }
 
-      // Vertical
-      for (int i = 0; i < board.length; i++) {
-         if (board[i][col] == 'Q') {
-            return false;
-         }
-      }
+        // Vertical
+        for (int i = 0; i < board.length; i++) {
+            if (board[i][col] == 'Q') {
+                return false;
+            }
+        }
 
-      // Upper left
-      for (int c = col, r = row; c >= 0 && r >= 0; c--, r--) {
-         if (board[r][c] == 'Q') {
-            return false;
-         }
-      }
+        // Upper left
+        for (int c = col, r = row; c >= 0 && r >= 0; c--, r--) {
+            if (board[r][c] == 'Q') {
+                return false;
+            }
+        }
 
-      // Upper right
-      for (int c = col, r = row; c < board.length && r >= 0; r--, c++) {
-         if (board[r][c] == 'Q') {
-            return false;
-         }
-      }
+        // Upper right
+        for (int c = col, r = row; c < board.length && r >= 0; r--, c++) {
+            if (board[r][c] == 'Q') {
+                return false;
+            }
+        }
 
-      // Lower left
-      for (int c = col, r = row; c >= 0 && r < board.length; r++, c--) {
-         if (board[r][c] == 'Q') {
-            return false;
-         }
-      }
+        // Lower left
+        for (int c = col, r = row; c >= 0 && r < board.length; r++, c--) {
+            if (board[r][c] == 'Q') {
+                return false;
+            }
+        }
 
-      // Lower right
-      for (int c = col, r = row; c < board.length && r < board.length; c++, r++) {
-         if (board[r][c] == 'Q') {
-            return false;
-         }
-      }
+        // Lower right
+        for (int c = col, r = row; c < board.length && r < board.length; c++, r++) {
+            if (board[r][c] == 'Q') {
+                return false;
+            }
+        }
 
-      return true;
-   }
+        return true;
+    }
 
-   /*
-    * Arrange n queens in a nxn chess board, such that they do not attack each
-    * other.
-    * Here we are iterating over board row since we are sure that every row has
-    * only one queen.
-    */
-   public static void main(String[] args) {
-      int n = 4;
-      char[][] board = new char[n][n];
+    /*
+     * Arrange n queens in a nxn chess board, such that they do not attack each other.
+     * Here we are iterating over board row since we are sure that every row has only one queen.
+     */
+    public static void main(String[] args) {
+        int n = 4;
+        char[][] board = new char[n][n];
 
-      for (int i = 0; i < n; i++) {
-         for (int j = 0; j < n; j++) {
-            board[i][j] = 'X';
-         }
-      }
-      nqueen(board, 0);
-   }
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                board[i][j] = 'X';
+            }
+        }
+        nQueen(board, 0);
+    }
 }
--- src/BackTracking/RatInADeadMazeFour.java
+++ src/BackTracking/RatInADeadMazeFour.java
@@ -2,53 +2,37 @@ package BackTracking;
 
 public class RatInADeadMazeFour {
 
-   private static void print(int sr, int sc, int er, int ec, String s, int[][] maze) {
-      if (sr < 0 || sc < 0)
-         return;
-      if (sc > ec || sr > er)
-         return;
-      if (sr == er && sc == ec) {
-         System.out.println(s);
-         return;
-      }
+    private static void print(int sr, int sc, int er, int ec, String s, int[][] maze) {
+        if (sr < 0 || sc < 0 || sc > ec || sr > er || maze[sr][sc] == 0 || maze[sr][sc] == -1)
+            return;
 
-      if (maze[sr][sc] == 0) // Blocked
-         return;
+        if (sr == er && sc == ec) {
+            System.out.println(s);
+            return;
+        }
 
-      if (maze[sr][sc] == -1) // Already Visited
-         return;
+        maze[sr][sc] = -1;
 
-      // Check Mark
-      maze[sr][sc] = -1;
+        print(sr, sc + 1, er, ec, s + "R", maze);
+        print(sr + 1, sc, er, ec, s + "D", maze);
+        print(sr, sc - 1, er, ec, s + "L", maze);
+        print(sr - 1, sc, er, ec, s + "U", maze);
 
-      // Go Right
-      print(sr, sc + 1, er, ec, s + "R", maze);
-      // Go Down
-      print(sr + 1, sc, er, ec, s + "D", maze);
-      // Go Left
-      print(sr, sc - 1, er, ec, s + "L", maze);
-      // Go Up
-      print(sr - 1, sc, er, ec, s + "U", maze);
+        maze[sr][sc] = 1;
+    }
 
-      // Backtracking
-      maze[sr][sc] = 1;
-   }
+    /*
+     * The rat can move through the maze in four directions, he has to move only if it is 1, if 0 can't move.
+     * We could have also created an isVisited 2-D matrix, but that would consume more memory.
+     * We have to apply a check because the rat would otherwise loop over the matrix.
+     */
+    public static void main(String[] args) {
+        int rows = 3;
+        int cols = 4;
+        int[][] maze = {{1, 0, 1, 1},
+                {1, 1, 1, 1,},
+                {1, 1, 0, 1}};
 
-   /*
-    * The rat can move through the maze in four directions but he has to move only
-    * if it is 1, if 0 can't move.
-    * We could have also created an isVisited 2-D matrix, but that would consume
-    * more memory.
-    * We have to apply a check because the rat would otherwise loop over the
-    * matrix.
-    */
-   public static void main(String[] args) {
-      int rows = 3;
-      int cols = 4;
-      int[][] maze = { { 1, 0, 1, 1 },
-            { 1, 1, 1, 1, },
-            { 1, 1, 0, 1 } };
-
-      print(0, 0, rows - 1, cols - 1, "", maze);
-   }
+        print(0, 0, rows - 1, cols - 1, "", maze);
+    }
 }
