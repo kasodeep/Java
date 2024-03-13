a7a9b117b477c0a64de46e570b64585a951c783c
Kasodariya Deep <106021230+kasodeep@users.noreply.github.com>
2024-03-13 12:52:39 +0530

    New Concepts Loaded!

--- src/ArraysD2/DiagonalTraversal.java
+++ src/ArraysD2/DiagonalTraversal.java
@@ -3,7 +3,6 @@ package ArraysD2;
 public class DiagonalTraversal {
 
     static int[] getDiagonal(int[][] matrix, int r, int c) {
-
         int[] ans = new int[r * c];
         int index = 0, row = 0, col = 0;
 
@@ -21,10 +20,45 @@ public class DiagonalTraversal {
         return ans;
     }
 
+    public static int[] matrixDiagonally(int[][] mat) {
+        int n = mat.length;
+        int[] res = new int[n * n];
+
+        int row = 0, col = 0;
+        for (int i = 0; i < n * n; i++) {
+            res[i] = mat[row][col];
+
+            // If sum of row and col is even, move up-right.
+            if ((row + col) % 2 == 0) {
+                if (col == n - 1) {
+                    row++;
+                } else if (row == 0) {
+                    col++;
+                } else {
+                    row--;
+                    col++;
+                }
+            }
+            // If sum of row and col is odd, move down-left.
+            else {
+                if (row == n - 1) {
+                    col++;
+                } else if (col == 0) {
+                    row++;
+                } else {
+                    row++;
+                    col--;
+                }
+            }
+        }
+
+        return res;
+    }
+
     public static void main(String[] args) {
         int[][] arr = {{1, 2, 3}, {4, 5, 6}, {7, 8, 9}};
         int[] diagonal = getDiagonal(arr, arr.length, arr[0].length);
-        for (int num: diagonal) {
+        for (int num : diagonal) {
             System.out.print(num + " ");
         }
     }
