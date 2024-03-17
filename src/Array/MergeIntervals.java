dd205af091f0f06547db4b95c8c2a21031486b5d
Kasodariya Deep <106021230+kasodeep@users.noreply.github.com>
2024-03-17 07:53:39 +0530

    Daily Improvements!

--- src/Array/MergeIntervals.java
+++ src/Array/MergeIntervals.java
@@ -45,4 +45,34 @@ public class MergeIntervals {
             System.out.println();
         }
     }
+
+    public static int[][] insert(int[][] intervals, int[] newInterval) {
+        List<int[]> result = new ArrayList<>();
+        int i = 0;
+
+        while (i < intervals.length && intervals[i][1] < newInterval[0]) {
+            result.add(intervals[i]);
+            i++;
+        }
+
+        while (i < intervals.length && intervals[i][0] <= newInterval[1]) {
+            // One condition change.
+            newInterval[0] = Math.min(newInterval[0], intervals[i][0]);
+            newInterval[1] = Math.max(newInterval[1], intervals[i][1]);
+            i++;
+        }
+        result.add(newInterval);
+
+        while (i < intervals.length) {
+            result.add(intervals[i]);
+            i++;
+        }
+
+        int[][] arr = new int[result.size()][2];
+        for (int j = 0; j < result.size(); j++) {
+            arr[j] = result.get(j);
+        }
+
+        return arr;
+    }
 }
