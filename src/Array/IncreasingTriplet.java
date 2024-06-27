a3f1d6df51367c65d8b9149cc32caed507d711c9
Kasodariya Deep <106021230+kasodeep@users.noreply.github.com>
2024-06-27 15:40:53 +0530

    Practice A Little, Play A Little.

---
+++ src/Array/IncreasingTriplet.java
@@ -0,0 +1,24 @@
+package Array;
+
+public class IncreasingTriplet {
+
+    public static boolean increasingTriplet(int[] nums) {
+        if (nums == null || nums.length < 3) {
+            return false;
+        }
+
+        int a = Integer.MAX_VALUE;
+        int b = Integer.MAX_VALUE;
+
+        for (int num : nums) {
+            if (num <= a) {
+                a = num;
+            } else if (num <= b) {
+                b = num;
+            } else {
+                return true;
+            }
+        }
+        return false;
+    }
+}
--- src/Stacks/SlidingWindow.java
+++ src/Stacks/SlidingWindow.java
@@ -1,5 +1,7 @@
 package Stacks;
 
+import java.util.ArrayDeque;
+import java.util.Deque;
 import java.util.Stack;
 
 public class SlidingWindow {
@@ -41,4 +43,28 @@ public class SlidingWindow {
         return ans;
     }
 
+    public int[] maxSlidingWindowTwo(int[] nums, int k) {
+        int n = nums.length;
+        if (n == 0 || k == 0) {
+            return new int[0];
+        }
+
+        int[] result = new int[n - k + 1];
+        Deque<Integer> win = new ArrayDeque<>();
+
+        for (int i = 0; i < n; ++i) {
+            while (win.size() > 0 && win.peekFirst() <= i - k) {
+                win.pollFirst();
+            }
+            while (win.size() > 0 && nums[win.peekLast()] < nums[i]) {
+                win.pollLast();
+            }
+            win.offerLast(i);
+
+            if (i >= k - 1) {
+                result[i - k + 1] = nums[win.peekFirst()];
+            }
+        }
+        return result;
+    }
 }
